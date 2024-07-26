fbb7717614a2e9befdaff17541089dfe3510eae3
sai-ramya-sri-darapureddy <ramyadarapureddy40.com>
2024-07-26 18:27:13 +0530

    implemented login,register, logout views and urls

--- auth_manager/views.py
+++ auth_manager/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from .forms import RegistrationForm
+from django.contrib import messages
 
 # Create your views here.
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, "Account created Successfully")
+            return redirect('login-user')
+    else:
+        form = RegistrationForm()
+    return render(request,'auth/register.html',{'form':form})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request,user)
+            messages.success(request, 'Login Successful')
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'auth/login.html',{'form':form})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
