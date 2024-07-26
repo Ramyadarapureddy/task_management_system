0762dfaae4eca3f8943a6ca930566939c46eab0a
sai-ramya-sri-darapureddy <ramyadarapureddy40.com>
2024-07-26 17:30:39 +0530

    implemented CustomUser

--- auth_manager/models.py
+++ auth_manager/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from auth_manager.managers import CustomUserManger
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    date_of_birth = models.DateField(null= True, blank=True)
+
+    objects = CustomUserManger()    
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
