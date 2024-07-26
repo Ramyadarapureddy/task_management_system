830e66b40023f0118ca6e894370836e7e5403df0
sai-ramya-sri-darapureddy <ramyadarapureddy40.com>
2024-07-26 17:37:36 +0530

    created RegistrationForm

---
+++ auth_manager/forms.py
@@ -0,0 +1,10 @@
+from django.forms import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import CustomUser
+
+class RegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2']
