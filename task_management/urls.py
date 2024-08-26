d4fb689c6de63cc90e7fa0fbdbfbf3e47d3b824b
sai-ramya-sri-darapureddy <ramyadarapureddy40.com>
2024-08-26 15:26:26 +0530

    created home and create_task urls

--- task_management/urls.py
+++ task_management/urls.py
@@ -1,1 +1,7 @@
-urlpatterns=[]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(' ', views.home_page, name='home'),
+    path('create_task',views.create_task, name='create_task'),
+]
